145965930c742dd646f48096bddc7b86d87042d6
tuanqatphs <144596640+tuanqatphs@users.noreply.github.com>
2023-10-10 18:29:35 +0800

    Delete score model directory

--- score model/R/goalmodel-package.R
+++
@@ -1,4 +0,0 @@
- ## usethis namespace: start
- #' @useDynLib goalmodel, .registration = TRUE
- ## usethis namespace: end
- NULL
--- score model/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(goalmodel)
-
-test_check("goalmodel")
